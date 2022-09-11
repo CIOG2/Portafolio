70477e8c26de296c8ce07d9b26cea0907dc2267c
Carlos Ivan <carlos.ivan10043@gmail.com>
2022-09-11 15:36:49 -0600

    Add: New description section

---
+++ Components/Header/DescriptionContent/index.tsx
@@ -0,0 +1,11 @@
+import Styles from './styles.module.scss';
+
+const DescriptionContent = () => {
+    return(
+        <section className={Styles.hola}>
+            
+        </section>
+        )
+    }
+
+export default DescriptionContent;
--- Components/Header/index.tsx
+++ Components/Header/index.tsx
@@ -1,12 +1,14 @@
 import Styles from './styles.module.scss';
 import NavBar from './NavBar';
 import Title from './Title';
+import DescriptionContent from './DescriptionContent';
 
 const Header = () => {
     return(
         <header className={Styles.header}>
             <NavBar/>
             <Title/>
+            <DescriptionContent/>
         </header>
         )
     }
