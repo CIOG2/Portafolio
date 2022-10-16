ec297bc7c56d1900084f906bb5836652a5d1b1fb
Carlos Ivan <carlos.ivan10043@gmail.com>
2022-10-16 16:20:31 -0600

    Fix: Best correction structure

--- Components/Header/Title/index.tsx
+++ Components/Header/Title/index.tsx
@@ -3,19 +3,17 @@ import Styles from './styles.module.scss';
 const Title = () => {
     
     return(
-        <section className={Styles.section}>
-             <div className={Styles.section__container}>
-                <h1 className={Styles.title}>
-                    <span className={Styles.title__text_1}>
-                        Front-end 
-                    </span>
-                        <br />
-                    <span className={Styles["title__text--2"]}>
-                        Developer Jr.
-                    </span>
-                </h1>
-            </div>
-        </section>
+        <div className={Styles["section__container"]}>
+            <h1 className={Styles["title"]}>
+                <span className={Styles["title__text_1"]}>
+                    Front-end 
+                </span>
+                    <br />
+                <span className={Styles["title__text--2"]}>
+                    Developer Jr.
+                </span>
+            </h1>
+        </div>
     )
 }
 
