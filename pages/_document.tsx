b0dc7c7b0d55abc49ea47d03000296bc18499b3a
Carlos Ivan <carlos.ivan10043@gmail.com>
2022-10-21 17:13:19 -0600

    Metadatos agregados

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,9 +16,25 @@ import Document, {
     render() {
       return (
         <Html lang='es'>
+            <title>Front-end Developer</title>
           <Head>
-            <meta name='author' content='CIOG' />
+            <meta name='author' content='Carlos Ivan Ochoa Gomez, CIOG' />
             <meta name='robots' content='index, follow' />
+
+            <meta name="title" content="CIOG - Frontend Developer"/>
+            <meta name="description" content="Hola soy Ivan un desarrollador web especializado en el front-end"/>
+
+            <meta property="og:type" content="website"/>
+            <meta property="og:url" content="https://www.ciog.dev/"/>
+            <meta property="og:title" content="CIOG - Frontend Developer"/>
+            <meta property="og:description" content="Hola soy Ivan un desarrollador web especializado en el front-end"/>
+            <meta property="og:image" content="https://i.ibb.co/DVQHdxb/Banner-Web.png"/>
+
+            <meta property="twitter:card" content="summary_large_image"/>
+            <meta property="twitter:url" content="https://www.ciog.dev/"/>
+            <meta property="twitter:title" content="CIOG - Frontend Developer"/>
+            <meta property="twitter:description" content="Hola soy Ivan un desarrollador web especializado en el front-end"/>
+            <meta property="twitter:image" content="https://i.ibb.co/DVQHdxb/Banner-Web.png"/>
           </Head>
           <body>
             <Main />
