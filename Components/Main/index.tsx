81960a673c6bbb023dfbceb99400d607ca4a6dee
Carlos Ivan <carlos.ivan10043@gmail.com>
2022-10-23 13:15:14 -0600

    Add: Main container

---
+++ Components/Main/index.tsx
@@ -0,0 +1,12 @@
+import Styles from './styles.module.scss';
+
+const Main = () => {
+        
+    return (
+        <div className = {Styles['hola']} >
+            <h1>Main</h1>
+        </div>
+    );
+}
+
+export default Main;
