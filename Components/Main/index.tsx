1f5259b5e2ae5c99102fff7bb02888824f0d59e9
Carlos Ivan <carlos.ivan10043@gmail.com>
2022-10-24 23:29:07 -0600

    Render main section

--- Components/Main/index.tsx
+++ Components/Main/index.tsx
@@ -1,11 +1,12 @@
+import AboutMe from './AboutMe';
 import Styles from './styles.module.scss';
 
 const Main = () => {
         
     return (
-        <div className = {Styles['hola']} >
-            <h1>Main</h1>
-        </div>
+        <main className = {Styles['main']} >
+            <AboutMe/>
+        </main>
     );
 }
 
