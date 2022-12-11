e3db29ebd9ea7368a0230f0489e091a3ac954462
Carlos ivan <carlos.ivan10043@gmail.com>
2022-12-11 15:02:10 -0700

    dd: favicons logo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,7 +16,7 @@ import Document, {
     render() {
       return (
         <Html lang='es'>
-            <title>Front-end Developer</title>
+            <title>CIOG - Frontend Developer</title>
           <Head>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com"/>
@@ -24,13 +24,19 @@ import Document, {
             <meta name='author' content='Carlos Ivan Ochoa Gomez, CIOG' />
             <meta name='robots' content='index, follow' />
 
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+            <link rel="shortcut icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+            <link rel="shortcut icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+            <link rel="shortcut icon" type="ico" href="/favicon/favicon.ico" sizes="32x32"/>
+
+
             <meta name="title" content="CIOG - Frontend Developer"/>
-            <meta name="description" content="Hola soy Ivan un desarrollador web especializado en el front-end"/>
+            <meta name="description" content="Hola soy Carlos Ivan un desarrollador web especializado en el desarrollo front-end"/>
 
             <meta property="og:type" content="website"/>
             <meta property="og:url" content="https://www.ciog.dev/"/>
             <meta property="og:title" content="CIOG - Frontend Developer"/>
-            <meta property="og:description" content="Hola soy Ivan un desarrollador web especializado en el front-end"/>
+            <meta property="og:description" content="Hola soy Carlos Ivan un desarrollador web especializado en el desarrollo front-end"/>
             <meta property="og:image" content="https://i.ibb.co/DVQHdxb/Banner-Web.png"/>
 
             <meta property="twitter:card" content="summary_large_image"/>
