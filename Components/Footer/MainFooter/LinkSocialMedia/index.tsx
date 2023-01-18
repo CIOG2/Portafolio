b0474a511b54d269020dd662fe76b0ce9659454c
Carlos ivan <carlos.ivan10043@gmail.com>
2023-01-17 22:26:41 -0700

    files LinkSocialMedia component

---
+++ Components/Footer/MainFooter/LinkSocialMedia/index.tsx
@@ -0,0 +1,11 @@
+import Styles from './styles.module.scss';
+
+const LinkSocialMedia = () => {
+    return (
+        <a href="">
+            
+        </a>
+    );
+}
+ 
+export default LinkSocialMedia;
