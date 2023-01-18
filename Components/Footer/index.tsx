a5d8555d366a37cf67e99e98b508f1aefd708f69
Carlos ivan <carlos.ivan10043@gmail.com>
2023-01-17 20:22:07 -0700

    Footer structure

---
+++ Components/Footer/index.tsx
@@ -0,0 +1,27 @@
+import Styles from './styles.module.scss';
+
+const Footer = () => {
+        
+    return (
+        <footer className = {Styles['footer']} >
+            <main className = {Styles['']}>
+                <div className = {Styles['']}>
+
+                </div>
+                <div className = {Styles['']}>
+
+                </div>
+                <div className = {Styles['']}>
+                    
+                </div>
+            </main>
+            <footer className = {Styles['']}>
+                <h3 className = {Styles['']}>
+
+                </h3>
+            </footer>
+        </footer>
+    );
+}
+
+export default Footer;
