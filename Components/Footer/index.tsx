ac120ff98abeec57a8dea99546a00b1c5728f6f3
Carlos ivan <carlos.ivan10043@gmail.com>
2023-01-21 23:55:32 -0700

    Component Footer

--- Components/Footer/index.tsx
+++ Components/Footer/index.tsx
@@ -1,25 +1,30 @@
+import MainFooter from './MainFooter';
 import Styles from './styles.module.scss';
+
 
 const Footer = () => {
         
     return (
         <footer className = {Styles['footer']} >
-            <main className = {Styles['']}>
+            <section className = {Styles['footer__section']}>
                 <div className = {Styles['']}>
-
+                    {/* <h4>Contacto</h4>
+                        <ul>
+                            <li>Culiacan</li>
+                            <li></li>
+                            <li></li>
+                        </ul> */}
                 </div>
-                <div className = {Styles['']}>
 
-                </div>
+                <MainFooter/>
+                
                 <div className = {Styles['']}>
                     
                 </div>
-            </main>
-            <footer className = {Styles['']}>
-                <h3 className = {Styles['']}>
-
-                </h3>
-            </footer>
+            </section>
+            <h3 className = {Styles['footer__footer--text']}>
+                © 2021 Creado y mas o menos diseñado por CIOG
+            </h3>
         </footer>
     );
 }
