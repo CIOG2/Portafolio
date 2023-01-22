37a9013597953272af59f418cb79e650a4d315a7
Carlos ivan <carlos.ivan10043@gmail.com>
2023-01-21 23:51:48 -0700

    Social media part of the footer

--- Components/Footer/MainFooter/LinkSocialMedia/index.tsx
+++ Components/Footer/MainFooter/LinkSocialMedia/index.tsx
@@ -1,9 +1,27 @@
+import Link from 'next/link';
+import { FC } from 'react';
 import Styles from './styles.module.scss';
 
-const LinkSocialMedia = () => {
+
+interface Props{
+    link: string;
+    linkImg: string;
+    alt: string;
+}
+
+const LinkSocialMedia:FC<Props> = ({link, linkImg, alt}) => {
     return (
-        <a href="">
-            
+        <a 
+            href= {link}
+            className = {Styles['link']}
+            rel = 'noopener noreferrer'
+            target = '_blank'   
+        >
+            <img 
+                className = {Styles['image']}
+                src={linkImg} 
+                alt={`foto de ${alt}`}
+            />
         </a>
     );
 }
