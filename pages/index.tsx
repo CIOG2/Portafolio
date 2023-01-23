7f88cca42eb8a4b4a2081c5baf83c3531c6744b5
Carlos ivan <carlos.ivan10043@gmail.com>
2023-01-22 23:52:55 -0700

    add: footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next'
-import Header from '../Components/Header';
-import Main from '../Components/Main';
+import Header from '@Components/Header';
+import Main from '@Components/Main';
+import Footer from '@Components/Footer';
 
 const Home: NextPage = () => {
   return (
     <div id = 'APP'>
           <Header/>
           <Main/>
+          <Footer/>
     </div>
   )
 }
