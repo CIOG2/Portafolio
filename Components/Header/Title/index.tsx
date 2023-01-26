d7c5ad61d8fc696834a61accbf7b316c37cfb87a
Carlos ivan <carlos.ivan10043@gmail.com>
2023-01-25 23:29:10 -0700

    fix: refactoring code

--- Components/Header/Title/index.tsx
+++ Components/Header/Title/index.tsx
@@ -5,11 +5,11 @@ const Title = () => {
     return(
         <div className = {Styles['section__container']}>
             <h1 className = {Styles['title']}>
-                <span className = {Styles['title__text--1']}>
+                <span className = {`${Styles['title__text']} ${Styles['title__text--1']}`}>
                     Front-end 
                 </span>
                     <br />
-                <span className = {Styles['title__text--2']}>
+                <span className = {`${Styles['title__text']} ${Styles['title__text--2']}`}>
                     Developer Jr.
                 </span>
             </h1>
